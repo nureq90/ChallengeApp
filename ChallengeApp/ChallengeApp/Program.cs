0536f4a2f571edf1748567d8e10b19e86520b295
nureq90 <p.p.kupczak90@gmail.com>
2023-04-06 22:33:43 +0200

    else if

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Pawel!");
+﻿string name = "Ewa";
+bool female = false;
+var age = 17;
+
+if (female == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (female == true && name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (female == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
